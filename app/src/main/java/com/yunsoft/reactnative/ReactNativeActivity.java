d0efd00919a3f50cb52713755a4f08b8993c6f20
yangyunfeng666 <445482919@qq.com>
2018-06-23 11:29:23 +0800

    添加图片

---
+++ app/src/main/java/com/yunsoft/reactnative/ReactNativeActivity.java
@@ -0,0 +1,17 @@
+package com.yunsoft.reactnative;
+
+import com.kye.reactnativeupdate.PreLoadReactActivity;
+
+/**
+ * Author: yangyunfeng
+ * Date: 公元2018-5-29 16:47
+ * Description:this is ReactNativeActivity
+ */
+public class ReactNativeActivity extends PreLoadReactActivity {
+
+    @Override
+    protected String getMainComponentName() {
+        return "test";
+    }
+
+}
